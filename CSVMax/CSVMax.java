4b6ffdb58a8cd860a0c812bf4cd66f61262660b5
Asim Zaidi <asimzaidih@gmail.com>
2017-07-06 18:20:22 -0500

    translated algorithm steps into code

--- CSVMax/CSVMax.java
+++ CSVMax/CSVMax.java
@@ -6,17 +6,33 @@
  * @version (a version number or a date)
  */
 import edu.duke.*;
-import org.apache.commmons.csv.*;
+import org.apache.commons.csv.*;
 import java.io.*;
 public class CSVMax {
     public CSVRecord hottestHourInFile(CSVParser parser) {
         //start with largestSoFar as nothing
-        
+        CSVRecord largestSoFar = null;
         //For each row (currentRow) in the CSV File
+        for (CSVRecord currentRow : parser) {
             //If largestSoFar is nothing
+            if (largestSoFar == null) {
+                largestSoFar = currentRow;
+            }
             //Otherwise
+            else {
+                double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+                double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
                 //Check if currentRow's temperature > largestSoFar
+                if (currentTemp > largestTemp)
+                {
                     //If so, update largestSoFar to currentRow
+                    largestSoFar = currentRow;
+                }
+            }
+        }
        //The largestSoFar is the an
+       return largestSoFar;
     }
+       //Files not given, so the test case code for this lecture coding series won't work D:
+    
 }
